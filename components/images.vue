b73a9894c302ccfe8de0cd6100190e36e2c39f44
maybeboar <maybeboar@gmail.com>
2024-03-10 16:25:30 +0300

    upd

--- components/images.vue
+++ components/images.vue
@@ -1,14 +1,11 @@
 <template>
     <div class="container">
-        <img class="image-1" src="/assets/material/is1.jpg">
+        <img  src="/assets/material/is1.jpg">
     </div>
 </template>
 
 <style lang="scss" scoped>
-.image-1 {
-    min-width: 1920px; 
-    height: 333px;     
-}
+
 .container {
   display: flex;
   flex-direction: row;
